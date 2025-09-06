b261d4c9ac399d44614163fa236774072f63f8c2
w69935 <114400981+WinterFoxH@users.noreply.github.com>
2025-09-06 14:17:52 +0200

    Testing and polishing

--- SFTP_Server/server.c
+++ SFTP_Server/server.c
@@ -1,44 +1,66 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <winsock2.h>
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    // Creating socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0) // IPv4 TCP socket
-    // Binding socket to the IP and port
+
+    WSADATA wsaData;
+    int wsaerr = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (wsaerr){
+          printf("WSAStartup failed\n");
+            return 1;  
+        }
+    
+    SOCKET server_fd = socket(AF_INET, SOCK_STREAM, 0); // IPv4 TCP socket
+    if (server_fd == INVALID_SOCKET){
+        printf("Socket creating failed.\n");
+        WSACleanup();
+        return 1;
+    }
+
     struct sockaddr_in address;
+    // Binding socket to the IP and port
     address.sin_family = AF_INET;
-    address.sin_addrp.s_addr = inet_addr("127.0.0.1"); // Localhost bind
+    address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Localhost bind
     address.sin_port = htons(8080); // Port 8080
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR){
+        printf("Bind failed. \n");
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
     // Listen for incomming connections
     listen(server_fd, 3); // Max of 3 pending connections
-
-    printf("Server is running and listening on port 8080...\n")
+    printf("Server is running and listening on port 8080...\n");
 
     while (1) {
         int addrlen = sizeof(address);
-        int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen); // Three way handshake with accept()
+        SOCKET new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen); // Three way handshake with accept()
 
-        if (new_socket < 0){
+        if (new_socket == INVALID_SOCKET){
             perror("Failed to accept connection");
             continue;
         }
         printf("Connection accepted.\n");
-    }
+    
     
     // Handling data transfer
     char buffer[1024] = {0};
-    read(new_socket, buffer, 1024); //Read data sent by the client
-    printf("Client message %s\n", buffer) // Print the client's message
+    recv(new_socket, buffer,  sizeof(buffer),0); //Read data sent by the client
+    printf("Client message %s\n", buffer); // Print the client's message
     
     // Respond to the client
     char *response = "Hello from the server.";
-    write(new_socket, response, strlen(response)); // Sending data to the client
+    send(new_socket, response, strlen(response), 0); // Sending data to the client
 
-    close(new_scocket); // Close the client connection
+    // Closing the client connection (Four-way handshake)
+    closesocket(new_socket); // Close the client connection
+    printf("Connection closed.\n");
 
 
+    }
+    // Close the listening socket (Will never be reached in current loop)
+    closesocket(server_fd);
+    WSACleanup();
+    return 0;
 }
